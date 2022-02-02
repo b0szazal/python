d17c893da1bf4b39086476f44af422971e1302cf
b0szazal <zalan.szasz06@gmail.com>
2022-02-02 12:34:11 +0100

    a

---
+++ 09 - Listák/IKT-4_talan/fizika.py
@@ -0,0 +1,55 @@
+from typing import *
+import time
+import os
+import random
+
+baloldal:List[int]=[1, 2, 3, 4, 5, 6]
+jobboldal:List[int]=[]
+helyzetek:List[str]=[]
+
+def hibakiiras(szoveg):
+    print(szoveg)
+    time.sleep(3)
+    os.system("cls")
+
+def szambeolvasas()->int:
+    eredmeny:int=None
+    while (eredmeny==None or eredmeny<0):
+        data:str=input("Kérem adja meg a dobások számát:  ")
+        if(data.replace("-", "").isnumeric()):
+            eredmeny=int(data)
+            if(eredmeny<0):
+                hibakiiras("Negatív számszor nem lehet dobni.")
+            else:
+                return eredmeny
+        else:
+            hibakiiras("Nem számot adott meg.")
+
+def dobas()->int:
+    eredmeny:int=random.randint(1, 6)
+    return eredmeny
+
+def kivetel(dobotszam:int, kivetel:List[int])->List[int]:
+    megtalalas:int=kivetel.index(dobottszam)
+    del(kivetel[megtalalas])
+    return kivetel
+
+def hozzaadas(dobotszam:int, hozzaadas:List[int])->List[int]:
+    hozzaadas.append(dobottszam)
+    return hozzaadas
+
+def helyzet(bal:List[int], jobb:List[int]):
+    print(f"A jelenlegi állás {len(bal)}-{len(jobb)}")
+    time.sleep(1)
+
+dobasokszama:int=szambeolvasas()
+for i in range(0, dobasokszama, 1):
+    dobottszam:int=dobas()
+    if (dobottszam in baloldal):
+        kivetel(dobottszam, baloldal)
+        hozzaadas(dobottszam, jobboldal)
+    else:
+        kivetel(dobottszam, jobboldal)
+        hozzaadas(dobottszam, baloldal)
+
+    helyzet(baloldal, jobboldal)
---
+++ 09 - Listák/test.py
@@ -0,0 +1,13 @@
+from typing import *
+
+lista:List[int]=[0, 1, 2]
+#teszt
+#törlés
+del(lista[1])
+lista.append(input("adjon egy számot  "))
+#mert kell
+szam:float=lista[2]
+print(szam)
+#rossz insert
+lista.insert(45, 10)
+print(lista[45])
